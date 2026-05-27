382a8bd9e9c304c4beea4e601c641f4bae761ff5
Faiz Lala <faiz4lala@gmail.com>
2026-05-27 12:17:22 +0530

    Added other btn for getting og Heading

--- Chaning Heading with Btn/script.js
+++ Chaning Heading with Btn/script.js
@@ -1,8 +1,14 @@
 let heading = document.getElementById("heading");
 let btn = document.querySelector("button");
+let btn2 = document.getElementById("og-btn");
 
 
 btn.addEventListener("click", function () {
     heading.innerHTML = "Heading Changed!";
     heading.style.color = "blue";   
 });
+
+btn2.addEventListener("click", function () {
+    heading.innerHTML = "Original Heading";
+    heading.style.color = "black";
+}); 
